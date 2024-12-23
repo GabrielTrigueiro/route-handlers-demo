9e7ec87285d972822b606ed164b4cee4ef725455
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2024-12-23 06:23:23 -0300

    tirando o cache de uma requisição

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,39 +1,32 @@
 import { redirect } from "next/navigation";
 import { comments } from "../data";
 
-export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
-  if (parseInt(params.id) > comments.length) {
+export type RequestProps = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function GET(_request: Request, { params }: RequestProps) {
+  const id = (await params).id;
+  if (parseInt(id) > comments.length) {
     redirect("/comments");
   }
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
 
-export async function PATCH(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(request: Request, { params }: RequestProps) {
+  const id = (await params).id;
   const body = await request.json();
   const { text } = body;
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   comments[index].text = text;
   return Response.json(comments[index]);
 }
 
-export async function DELETE(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+export async function DELETE(_request: Request, { params }: RequestProps) {
+  const id = (await params).id;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   comments.splice(index, 1);
   return Response.json(comments);
 }
