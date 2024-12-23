3c9de4aec344825415427599f00af77b1385a96a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2024-12-23 06:00:11 -0300

    formas de pegar o header da requisição

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
-  return new Response("Profile Data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+  // ? Duas formas de pegar o header de uma requisição
+  console.log(requestHeaders.get("authorization"));
+  console.log(headerList);
+  return new Response("Hello from the API");
 }
